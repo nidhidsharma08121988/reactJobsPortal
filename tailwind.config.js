5db4c99029562408c5e7630c29094ee4ee29311c
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-15 15:49:37 +0400

    setup completed - initial commmit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto','sans-serif']
+      },
+      gridTemplateColumns
+    },
+  },
+  plugins: [],
+}
+
