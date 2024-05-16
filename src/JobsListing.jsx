9681e1406e5190e9750d34e8f957b17b76900c5a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-16 16:05:14 +0400

    remove consol.log

--- src/JobsListing.jsx
+++ src/JobsListing.jsx
@@ -1,8 +1,10 @@
 import jobs from "./jobsListings.json";
 
 const allJobs = jobs.jobs;
-console.log(allJobs)
-export const JobsListing = ()=><section className="bg-blue-50 px-4 py-10">
+
+export const JobsListing = ()=>{
+ 
+  return (<section className="bg-blue-50 px-4 py-10">
   <div className="container-xl lg:container m-auto">
     <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
       Browse Jobs
@@ -42,4 +44,4 @@ export const JobsListing = ()=><section className="bg-blue-50 px-4 py-10">
       ))}
     </div>
   </div>
-</section>;
+</section>)};
