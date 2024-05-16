47ca2fc1bcabe70ddca5d7d8d15b3e31499383dc
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-16 12:36:25 +0400

    Card: props for background

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,8 @@
 
 import { PropTypes } from 'prop-types';
 
-const Card = ({children, bg="bg-gray-100"}) => {
-  return <div className="bg-gray-100 p-6 rounded-lg shadow-md">{children}</div>;
+const Card = ({children, bg = "bg-gray-100"}) => {
+  return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 };
 
 export default Card;
