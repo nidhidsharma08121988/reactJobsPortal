245f1ce6ef7da615a21afffadf9b189748a16190
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-16 11:24:00 +0400

    convert NavBar to arrow function component

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,6 +1,6 @@
 import logo from "./assets/react.svg"
 
-export const NavBar = <nav className="bg-indigo-700 border-b border-indigo-500">
+export const NavBar =()=> <nav className="bg-indigo-700 border-b border-indigo-500">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
     <div className="flex h-20 items-center justify-between">
       <div
