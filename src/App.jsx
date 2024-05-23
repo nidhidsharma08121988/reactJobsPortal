7e0c86758ff5623528a44dc9a28aded8911e8335
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-23 11:29:30 +0400

    Router: Add route, routers & provider to the file

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import { HomeCards } from "./HomeCards";
-import { Hero } from "./Hero";
-import { NavBar } from "./NavBar";
-import { JobsListing } from "./JobsListings";
-import ViewAllJobs from "./ViewAllJobs";
 
+import {createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
+import MainLayout from './layouts/MainLayout';
+import JobsPage from './pages/JobsPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout/>}>
+    <Route index element={<HomePage/>}/>
+    <Route path="/jobs" element={<JobsPage/>}/>
+  </Route>
+
+  )
+)
 
 
 
 const App = () => {
   
-  return (<>
-    <NavBar/>
-
-    {/* <!-- Hero --> */}
-    <Hero/>
-
-    {/* <!-- Developers and Employers --> */}
-    <HomeCards/>
-
-    {/* <!-- Browse Jobs --> */}
-    <JobsListing/>
-
-    <ViewAllJobs/>
-
-  </>);
+  return (<RouterProvider router={router}>
+  </RouterProvider>);
 };
 
 export default App;
