c5a59a66cbbbc354b9808dbf9403492199d5869d
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-23 11:35:00 +0400

    Replace a tag with Link tag of router

--- src/JobListing.jsx
+++ src/JobListing.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 import  {useState} from 'react';
 import {FaMapMarker} from 'react-icons/fa'
+import { Link } from "react-router-dom";
  function JobListing({job}) {
 
   const [showFullDescription, setShowFullDescription] = useState(false)
@@ -35,12 +36,12 @@ import {FaMapMarker} from 'react-icons/fa'
           <FaMapMarker className="inline text-lg  mr-1 mb-1"/>
           {job.location}
         </div>
-        <a
+        <Link
           href={`/job/${job.id}`}
           className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
-        </a>
+        </Link>
       </div>
     </div>
   </div>;
