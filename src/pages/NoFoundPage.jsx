731691a99cb5054a066f50a67fd76161005fecd3
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 11:09:48 +0400

    create not found page

---
+++ src/pages/NoFoundPage.jsx
@@ -0,0 +1,13 @@
+
+import { Link } from "react-router-dom";
+
+const NoFoundPage = () => {
+  return <section className="text-center flex flex-col justify-center">
+    <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
+    <h1 className="text-6xl font-bold mb-4">40 Not Found</h1>
+    <p className="text-xl mb-5">This page does not exist</p>
+    <Link to="/" className="text-white bg-indigo-700 hover:indigo-900 rounded px-3 py-2 mt-4">Go Back</Link>
+  </section>;
+};
+
+export default NoFoundPage;
