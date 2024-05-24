2890e637d05532678c39e15bb72f5cf3a7d592d5
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 11:33:08 +0400

    Css: add classes to modify look of page and alignment of content

--- src/pages/NoFoundPage.jsx
+++ src/pages/NoFoundPage.jsx
@@ -1,12 +1,13 @@
 
 import { Link } from "react-router-dom";
 import { FaExclamationTriangle } from "react-icons/fa";
+
 const NoFoundPage = () => {
-  return <section className="text-center flex flex-col justify-center">
-    <FaExclamationTriangle className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"/>
-    <h1 className="text-6xl font-bold mb-4">40 Not Found</h1>
-    <p className="text-xl mb-5">This page does not exist</p>
-    <Link to="/" className="text-white bg-indigo-700 hover:indigo-900 rounded px-3 py-2 mt-4">Go Back</Link>
+  return <section className="text-center flex flex-col align-middle justify-items-center items-center gap-5 mt-5">
+    <FaExclamationTriangle className="fas fa-exclamation-triangle text-4xl text-yellow-400 fa-4x mb-4"/>
+    <h1 className="text-6xl font-bold ">40 Not Found</h1>
+    <p className="text-xl ">This page does not exist</p>
+    <Link to="/" className="text-white bg-indigo-700 justify-self-center  hover:indigo-900 rounded w-25  px-3 py-2 ">Go Back</Link>
   </section>;
 };
 
