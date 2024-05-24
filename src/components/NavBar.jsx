95dac3cb11429d71691b5734cf79dc07e4a64adc
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 11:53:11 +0400

    Link -> NavLink to assign className based on whether a link is active or no

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
-import logo from "./assets/react.svg"
-import {Link} from "react-router-dom"
+import logo from "../assets/react.svg"
+import {NavLink} from "react-router-dom"
+
+const getClassNames = ({ isActive }) => isActive ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
 
 export const NavBar =()=> <nav className="bg-indigo-700 border-b border-indigo-500">
   <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -8,28 +10,28 @@ export const NavBar =()=> <nav className="bg-indigo-700 border-b border-indigo-5
         className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
       >
         {/* <!-- Logo --> */}
-        <Link className="flex flex-shrink-0 items-center mr-4" to="/">
+        <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
           <img
             className="h-10 w-auto"
             src={logo}
             alt="React Jobs" />
           <span className="hidden md:block text-white text-2xl font-bold ml-2"
           >React Jobs</span>
-        </Link>
+        </NavLink>
         <div className="md:ml-auto">
           <div className="flex space-x-2">
-            <Link
+            <NavLink
               to="/"
-              className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-            >Home</Link>
-            <Link
+              className={getClassNames}
+            >Home</NavLink>
+            <NavLink
               to="/jobs"
-              className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-            >Jobs</Link>
-            <Link
+              className={getClassNames}
+            >Jobs</NavLink>
+            <NavLink
               to="/add-job"
-              className="text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-            >Add Job</Link>
+              className={ getClassNames}
+            >Add Job</NavLink>
           </div>
         </div>
       </div>
