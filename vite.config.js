1cf1d742557e5265b85943ef8486d8bd45452cd0
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 17:23:02 +0400

    add proxy to our vite.config file - allows to use /api instead of localhost 5000

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server:{
     port: 3000,
-  }
+    proxy:{
+      '/api':{
+        changeOrigin: true,
+        target:"http://localhost:5000",
+        rewrite: (path) => path.replace(/^\/api/,""),
+      },
+    },
+  },
 })
