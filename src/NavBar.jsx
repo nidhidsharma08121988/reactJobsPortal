209a5c51fc6900b639d2303fe170fc9989a22b9d
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 10:37:36 +0400

    href to to

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,7 +8,7 @@ export const NavBar =()=> <nav className="bg-indigo-700 border-b border-indigo-5
         className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
       >
         {/* <!-- Logo --> */}
-        <Link className="flex flex-shrink-0 items-center mr-4" to="/index">
+        <Link className="flex flex-shrink-0 items-center mr-4" to="/">
           <img
             className="h-10 w-auto"
             src={logo}
