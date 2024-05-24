1ee7834a7416dacb5904bf0c3bf0c6c5667cb729
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 17:24:40 +0400

    Replace code with <JobsListing> and isHome to be sre whether its home page or no

--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -1,19 +1,9 @@
-
-import jobs from "../jobListings.json"
-import JobListing from "../components/JobListing";
-const allJobs = jobs.jobs;
+import { JobsListing } from "../components/JobsListings";
 
 const JobsPage = () => {
   return <section className="bg-blue-50 px-4 py-10">
   <div className="container-xl lg:container m-auto">
-    <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-      Browse Jobs
-    </h2>
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-      {allJobs.map((job)=>(
-         <JobListing key={job.id} job={job}/>
-      ))}
-    </div>
+   <JobsListing isHome={false}/>
   </div>
 </section>;
 };
