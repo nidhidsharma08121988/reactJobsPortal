6e544b1570ebaacfe2dbfe589c0c5855f0e661ae
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 11:10:46 +0400

    add font awesome exclaimation icon

--- src/pages/NoFoundPage.jsx
+++ src/pages/NoFoundPage.jsx
@@ -1,9 +1,9 @@
 
 import { Link } from "react-router-dom";
-
+import { FaExclamationTriangle } from "react-icons/fa";
 const NoFoundPage = () => {
   return <section className="text-center flex flex-col justify-center">
-    <i className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"></i>
+    <FaExclamationTriangle className="fas fa-exclamation-triangle text-yellow-400 fa-4x mb-4"/>
     <h1 className="text-6xl font-bold mb-4">40 Not Found</h1>
     <p className="text-xl mb-5">This page does not exist</p>
     <Link to="/" className="text-white bg-indigo-700 hover:indigo-900 rounded px-3 py-2 mt-4">Go Back</Link>
