20ace0dd21950a4d253fe32da07f3fcafe1dbef4
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2024-05-24 10:38:00 +0400

    href to to

--- src/JobListing.jsx
+++ src/JobListing.jsx
@@ -37,7 +37,7 @@ import { Link } from "react-router-dom";
           {job.location}
         </div>
         <Link
-          href={`/job/${job.id}`}
+          to={`/job/${job.id}`}
           className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
         >
           Read More
